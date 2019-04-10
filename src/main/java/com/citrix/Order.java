1bbbc8f7e73c2641e010655d60c499379da91cc3
dmAdmin <>
2019-04-10 16:17:09 +0000

    {/src/main/java/com/citrix/Order.java}

--- src/main/java/com/citrix/Order.java
+++ src/main/java/com/citrix/Order.java
@@ -6,12 +6,37 @@ package com.citrix;
 
 public class Order implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Order() {
-    }
+	@org.kie.api.definition.type.Label(value = "Order Header")
+	private com.citrix.OrderHeader orderHeader;
+	@org.kie.api.definition.type.Label(value = "Order Line Items")
+	private java.util.List<com.citrix.OrderLineItem> orderLineItems;
 
+	public Order() {
+	}
 
+	public com.citrix.OrderHeader getOrderHeader() {
+		return this.orderHeader;
+	}
 
+	public void setOrderHeader(com.citrix.OrderHeader orderHeader) {
+		this.orderHeader = orderHeader;
+	}
+
+	public java.util.List<com.citrix.OrderLineItem> getOrderLineItems() {
+		return this.orderLineItems;
+	}
+
+	public void setOrderLineItems(
+			java.util.List<com.citrix.OrderLineItem> orderLineItems) {
+		this.orderLineItems = orderLineItems;
+	}
+
+	public Order(com.citrix.OrderHeader orderHeader,
+			java.util.List<com.citrix.OrderLineItem> orderLineItems) {
+		this.orderHeader = orderHeader;
+		this.orderLineItems = orderLineItems;
+	}
 
 }
