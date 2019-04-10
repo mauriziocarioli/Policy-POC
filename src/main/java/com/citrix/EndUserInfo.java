7c88b1518eea8fd48e8acab51595a7e219e9d4ea
dmAdmin <>
2019-04-10 21:51:54 +0000

    {/src/main/java/com/citrix/EndUserInfo.java}

--- src/main/java/com/citrix/EndUserInfo.java
+++ src/main/java/com/citrix/EndUserInfo.java
@@ -6,6 +6,11 @@ public class EndUserInfo implements java.io.Serializable {
     
     private java.lang.String end_User_Org_ID;
     private java.lang.String end_User_Geo;
+    private java.lang.String end_User_Area;
+    private java.lang.String end_User_Territory;
+    private java.lang.String end_User_Region;   
+    private java.lang.String end_User_Email_Address;    
+    private java.lang.String high_Touch_Flag;
 
     public java.lang.String getEnd_User_Org_ID() {
         return this.end_User_Org_ID;
@@ -23,9 +28,54 @@ public class EndUserInfo implements java.io.Serializable {
         this.end_User_Geo = end_User_Geo;
     }
 
-    public EndUserInfo(java.lang.String end_User_Org_ID, java.lang.String end_User_Geo) {
+    public java.lang.String getEnd_User_Area() {
+        return this.end_User_Area;
+    }
+
+    public void setEnd_User_Area(java.lang.String end_User_Area) {
+        this.end_User_Area = end_User_Area;
+    }
+
+    public java.lang.String getEnd_User_Territory() {
+        return this.end_User_Territory;
+    }
+
+    public void setEnd_User_Territory(java.lang.String end_User_Territory) {
+        this.end_User_Territory = end_User_Territory;
+    }
+
+    public java.lang.String getEnd_User_Region() {
+        return this.end_User_Region;
+    }
+
+    public void setEnd_User_Region(java.lang.String end_User_Region) {
+        this.end_User_Region = end_User_Region;
+    }
+
+    public java.lang.String getEnd_User_Email_Address() {
+        return this.end_User_Email_Address;
+    }
+
+    public void setEnd_User_Email_Address(java.lang.String end_User_Email_Address) {
+        this.end_User_Email_Address = end_User_Email_Address;
+    }
+
+    public java.lang.String getHigh_Touch_Flag() {
+        return this.high_Touch_Flag;
+    }
+
+    public void setHigh_Touch_Flag(java.lang.String high_Touch_Flag) {
+        this.high_Touch_Flag = high_Touch_Flag;
+    }
+
+    public EndUserInfo(java.lang.String end_User_Org_ID, java.lang.String end_User_Geo, java.lang.String end_User_Area, java.lang.String end_User_Territory, java.lang.String end_User_Region, java.lang.String end_User_Email_Address, java.lang.String high_Touch_Flag) {
         this.end_User_Org_ID = end_User_Org_ID;
         this.end_User_Geo = end_User_Geo;
+        this.end_User_Area = end_User_Area;
+        this.end_User_Territory = end_User_Territory;
+        this.end_User_Region = end_User_Region;
+        this.end_User_Email_Address = end_User_Email_Address;
+        this.high_Touch_Flag = high_Touch_Flag;
     }
 
 }
