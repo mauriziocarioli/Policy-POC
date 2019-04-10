0078183293162c90235785ec2bdf63751491e780
dmAdmin <>
2019-04-10 20:40:42 +0000

    {/src/main/java/com/citrix/AuthorizationItem.java}

--- src/main/java/com/citrix/AuthorizationItem.java
+++ src/main/java/com/citrix/AuthorizationItem.java
@@ -4,28 +4,28 @@ public class AuthorizationItem implements java.io.Serializable {
 
     static final long serialVersionUID = 1L;
     
-    private java.lang.String Product_Line_ID;
-    private java.lang.String Cert_Status;
+    private java.lang.String product_Line_ID;
+    private java.lang.String cert_Status;
 
     public java.lang.String getProduct_Line_ID() {
-        return this.Product_Line_ID;
+        return this.product_Line_ID;
     }
 
-    public void setProduct_Line_ID(java.lang.String Product_Line_ID) {
-        this.Product_Line_ID = Product_Line_ID;
+    public void setProduct_Line_ID(java.lang.String product_Line_ID) {
+        this.product_Line_ID = product_Line_ID;
     }
 
     public java.lang.String getCert_Status() {
-        return this.Cert_Status;
+        return this.cert_Status;
     }
 
-    public void setCert_Status(java.lang.String Cert_Status) {
-        this.Cert_Status = Cert_Status;
+    public void setCert_Status(java.lang.String cert_Status) {
+        this.cert_Status = cert_Status;
     }
 
-    public AuthorizationItem(java.lang.String Product_Line_ID, java.lang.String Cert_Status) {
-        this.Product_Line_ID = Product_Line_ID;
-        this.Cert_Status = Cert_Status;
+    public AuthorizationItem(java.lang.String product_Line_ID, java.lang.String cert_Status) {
+        this.product_Line_ID = product_Line_ID;
+        this.cert_Status = cert_Status;
     }
 
 }    
